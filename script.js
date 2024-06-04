1322aa0b32794507ed97a31fcea2da1170f42de1
4mshu <2hungrygators@gmail.com>
2024-06-04 17:48:44 +0545

    finished code for store, writing for fighting

--- script.js
+++ script.js
@@ -15,19 +15,66 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+    {
+        name : "stick",
+        power: 5
+    },
+    {
+        name : "dagger",
+        power: 30
+    },
+    {
+        name : "claw hammer",
+        power: 50
+    },
+    {
+        name : "sword",
+        power: 100
+    }
+];
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    }
+];
 const locations = [
     {
         name : "town square",
         "button text" : ["Go to store", "Go to cave", "Fight dragon"],
         "button functions" : [goStore, goCave, fightDragon],
         text : "You are in the town square. You see a sign that says \"Store\"."
-},
-{
-    name : "store",
-    "button text" : ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-    "button functions" : [buyHealth, buyWeapon, goTown],
-    text : "You enter the store."
-}
+    },
+    {
+        name : "store",
+        "button text" : ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions" : [buyHealth, buyWeapon, goTown],
+        text : "You enter the store."
+    },
+    {
+        name : "cave",
+        "button text" : ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions" : [fightSlime, fightBeast, goTown],
+        text : "You enter the cave. You see some monsters."
+    },
+    {
+        name : "fight",
+        "button text" : ["Attack", "Dodge", "Run"],
+        "button functions" : [attack, dodge, goTown],
+        text : "You are fighting a monster."
+    }
 ];
 
 //initialize buttons
@@ -52,26 +99,71 @@ function goStore(){
     update(locations[1]);
 }
 function goCave(){
-    return console.log("Going to cave.");
+    update(locations[2]);
 }
-function fightDragon(){
-    return console.log("Fighting dragon.");
-}
-
 function buyHealth(){
-
+    if (gold>=10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health;
+    } else{
+        text.innerText  =  "You do not have enough gold to buy health.";  
+    }
 }
-function buyWeapon(){
 
+
+function buyWeapon(){
+    if (currentWeaponIndex<weapons.length-1){
+        if(gold>=30){
+            gold -= 30;
+            currentWeaponIndex++;
+            goldText.innerText = gold;
+            let newWeapon = weapons[currentWeaponIndex].name;
+            text.innerText = "You now have a "+newWeapon+".";
+            inventory.push(newWeapon);
+            text.innerText += " In your inventory you have: " + inventory;
+        } else{
+            text.innerText = "You do not have enough gold to buy a weapon.";
+        }
+    } else{
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+}
+
+function sellWeapon(){
+    if (inventory.length>1){
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a "+currentWeapon+".";
+        text.innerText += " In your inventory you have: "+inventory;
+    } else{
+        text.innerText = "Don't sell your only weapon!";
+    }
 }
 
 function fightSlime(){
-    
+    fighting = 0;
+    goFight();
 }
 function fightBeast(){
+    fighting = 1;
+    goFight();
+}
+function fightDragon(){
+    fighting = 2;
+    goFight();
+}
+function goFight(){
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+}
+function attack(){
 
 }
+function dodge(){
 
-
-
-
+}
