56bc34ef9600b513e66b9a6d2723a179bad87847
4mshu <2hungrygators@gmail.com>
2024-06-09 09:27:55 +0545

    function and logic for determining who won

--- Rock, Paper, Scissors/script.js
+++ Rock, Paper, Scissors/script.js
@@ -1,5 +1,31 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-  
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
   }
-  console.log(getRandomComputerResult());
+  
+  let playerScore = 0;
+  let computerScore = 0;
+  
+  function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Rock" && computer === "Scissors") ||
+      (player === "Scissors" && computer === "Paper") ||
+      (player === "Paper" && computer === "Rock")
+    );
+  }
+  function getRoundResults(userOption) {
+    const computerResult = getRandomComputerResult();
+    if (hasPlayerWonTheRound(userOption,computerResult)){
+        playerScore++;
+        return document.getElementById('results-msg').innerHTML = `Player wins! ${userOption} beats ${computerResult}`;
+    } else if(userOption == computerResult){
+        return document.getElementById('results-msg').innerHTML = `It's a tie! Both chose ${userOption}`;
+    } else{
+        computerScore++;
+        return document.getElementById('results-msg').innerHTML = `Computer wins! ${computerResult} beats ${userOption}`;
+    }
+  }
+  
+  console.log(getRoundResults("Rock"));
+  console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
