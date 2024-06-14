1dad52b179f0f704003138815c97faf7351e77fa
4mshu <2hungrygators@gmail.com>
2024-06-14 17:22:13 +0545

    cleaner code before recursion

--- Binary Converter/script.js
+++ Binary Converter/script.js
@@ -1,37 +1,42 @@
+
+
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-    const inputs = [];
-    const quotients = [];
-    const remainders = [];
-    
-    while(input>0){
-        let quotient = Math.floor(input/2);
-        let remainder = input % 2;
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-        input = quotient;
-    }
-    console.log("Inputs: ", inputs);
-    console.log("Quotients: ", quotients);
-    console.log("Remainders: ", remainders);
-    result.innerText = remainders.reverse().join("");
-};
-const checkUserInput = () => {
-    if(!numberInput.value || isNaN(parseInt(numberInput.value)) || parseInt(numberInput.value) < 0){
-        alert("Please provide a decimal number greater than or equal to 0");
-        return;
-    }
-    decimalToBinary(parseInt(numberInput.value));
-    numberInput.value = "";
-}
-convertBtn.addEventListener("click", checkUserInput);
-numberInput.addEventListener("keydown", (e)=>{
-    if (e.key=="Enter"){
-        checkUserInput();
-    }
-})
+  let binary = "";
 
+  if (input === 0) {
+    binary = "0";
+  }
+
+  while (input > 0) {
+    binary = (input % 2) + binary;
+    console.log(binary);
+    input = Math.floor(input / 2);
+  }
+  result.innerText = binary;
+};
+
+const checkUserInput = () => {
+  if (
+    !numberInput.value ||
+    isNaN(parseInt(numberInput.value)) ||
+    parseInt(numberInput.value) < 0
+  ) {
+    alert("Please provide a decimal number greater than or equal to 0");
+    return;
+  }
+
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+};
+
+convertBtn.addEventListener("click", checkUserInput);
+
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkUserInput();
+  }
+});
