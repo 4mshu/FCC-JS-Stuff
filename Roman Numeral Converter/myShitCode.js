80ed50c75cdc94644deafab6ebb6ecf800e64ed8
4mshu <2hungrygators@gmail.com>
2024-06-17 20:50:11 +0545

    need to write new code for my shitty code

---
+++ Roman Numeral Converter/myShitCode.js
@@ -0,0 +1,5 @@
+const numberInput = document.getElementById("number");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("output");
+
+//i'm gonna write this on my free time again
