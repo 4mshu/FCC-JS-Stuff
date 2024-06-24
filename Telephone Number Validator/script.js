e3f5e9c5c416516dcf63212603b9cd2acb003029
4mshu <2hungrygators@gmail.com>
2024-06-25 00:43:00 +0545

    regexes written, edge cases remaining

--- Telephone Number Validator/script.js
+++ Telephone Number Validator/script.js
@@ -1,25 +1,30 @@
 const checkBtn = document.getElementById("check-btn")
 const clearBtn = document.getElementById("clear-btn")
-
-const checkPhoneNumber = (number) =>{
-    
+const checkPhoneNumber = (number) => {
+    const regex = /^1?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/
+    const regex2 = /^1 \(\d{3}\) \d{3}-\d{4}/
+    const resultsDiv =document.getElementById("results-div")
+    if(regex.test(number)){
+        resultsDiv.innerText = `Valid US number: ${number}`
+    } 
+    else{
+        resultsDiv.innerText = `Invalid US number: ${number}`
+    }
 }
-
 const checkUserInput = () =>{
     const userInput = document.getElementById("user-input")
     if(!userInput.value){
         alert("Please provide a phone number");
-    }
-    else {
+    } else{
         checkPhoneNumber(userInput.value);
-    }
+    } 
     userInput.value="";
 }
 const clearInput =() =>{
     const userInput = document.getElementById("user-input")
-    const resultsDiv = document.getElementById("results-div")
+    const resultsDiv =document.getElementById("results-div")
     userInput.value="";
-    resultsDiv.value="";
+    resultsDiv.innerHTML = "";   
 }
 
 checkBtn.addEventListener("click", checkUserInput)
