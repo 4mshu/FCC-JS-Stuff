af0a0ba3567e2b8f79edaf9b7658541067951bb8
4mshu <2hungrygators@gmail.com>
2024-06-27 14:26:04 +0545

    radio button updation logic

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -1,39 +1,88 @@
-const listOfAllDice = document.querySelectorAll(".die")
-const scoreInputs = document.querySelectorAll("#score-options input")
-const scoreSpans = document.querySelectorAll("#score-options span")
-const currentRoundText = document.getElementById("current-round")
-const currentRoundRollsText = document.getElementById("current-round-rolls")
-const totalScoreText = document.getElementById("total-score")
-const scoreHistory = document.getElementById("score-history")
-const rollDiceBtn = document.getElementById("roll-dice-btn")
-const keepScoreBtn = document.getElementById("keep-score-btn")
-const rulesContainer = document.querySelector(".rules-container")
-const rulesBtn = document.getElementById("rules-btn")
+const listOfAllDice = document.querySelectorAll(".die");
+const scoreInputs = document.querySelectorAll("#score-options input");
+const scoreSpans = document.querySelectorAll("#score-options span");
+const currentRound = document.getElementById("current-round");
+const currentRoundRolls = document.getElementById("current-round-rolls");
+const totalScore = document.getElementById("total-score");
+const scoreHistory = document.getElementById("score-history");
+const rollDiceBtn = document.getElementById("roll-dice-btn");
+const keepScoreBtn = document.getElementById("keep-score-btn");
+const rulesContainer = document.querySelector(".rules-container");
+const rulesBtn = document.getElementById("rules-btn");
 
-let isModalShowing = false;
 let diceValuesArr = [];
-let rolls = 0;
+let isModalShowing = false;
 let score = 0;
-let totalScore = 0;
+let total = 0;
 let round = 1;
+let rolls = 0;
 
 const rollDice = () => {
-    diceValuesArr = [];
-    for(let i=0;i<5;i++){
-        const randomDice = Math.floor((Math.random()*6)+1);
-        diceValuesArr.push(randomDice);
-    }
-    listOfAllDice.forEach((dice,index)=>{})
+  diceValuesArr = [];
+
+  for (let i = 0; i < 5; i++) {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    diceValuesArr.push(randomDice);
+  };
+
+  listOfAllDice.forEach((dice, index) => {
+    dice.textContent = diceValuesArr[index];
+  });
 };
 
-rulesBtn.addEventListener("click", ()=>{
-    isModalShowing = !isModalShowing;
-    if(isModalShowing){
-        rulesBtn.textContent = "Hide Rules";
-        rulesContainer.style.display = "block";
-    } else{
-        rulesBtn.textContent = "Show Rules";
-        rulesContainer.style.display = "none";
-    }
+const updateStats = () => {
+  currentRoundRolls.textContent = rolls;
+  currentRound.textContent = round;
+};
 
-})
+const updateRadioOption = (index, score) => {
+  scoreInputs[index].disabled = false;
+  scoreInputs[index].value = score;
+  scoreSpans[index].textContent = `, score = ${score}`;
+};
+
+const getHighestDuplicates = (arr) =>{
+    let counts ={};
+    let highestCount = 0 
+    for (let num of arr) {
+        if (counts[num]) {
+            counts[num]++;
+        } else {
+            counts[num] = 1;
+        }
+        if(counts[num]>highestCount){
+            highestCount = counts[num]
+        }
+    }
+   if(highestCount >= 4){
+    updateRadioOption(1, arr.reduce((acc,el)=>acc+el,0));
+    updateRadioOption(0, arr.reduce((acc,el)=>acc+el,0));
+   } else if (highestCount >= 3){
+    updateRadioOption(0, arr.reduce((acc,el)=>acc+el,0));
+   } else {
+    updateRadioOption(5, 0)
+   }
+};
+
+rollDiceBtn.addEventListener("click", () => {
+  if (rolls === 3) {
+    alert("You have made three rolls this round. Please select a score.");
+  } else {
+    rolls++;
+    rollDice();
+    updateStats();
+    getHighestDuplicates(diceValuesArr);
+  }
+});
+
+rulesBtn.addEventListener("click", () => {
+  isModalShowing = !isModalShowing;
+
+  if (isModalShowing) {
+    rulesBtn.textContent = "Hide Rules";
+    rulesContainer.style.display = "block";
+  } else {
+    rulesBtn.textContent = "Show Rules";
+    rulesContainer.style.display = "none";
+  }
+});
