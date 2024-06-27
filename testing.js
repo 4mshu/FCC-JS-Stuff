caf64f04aa838ee32bfba4fc6b3c1e8c8495524c
4mshu <2hungrygators@gmail.com>
2024-06-27 11:43:46 +0545

    dice game wip, random dice logic

--- testing.js
+++ testing.js
@@ -1,16 +1,1 @@
-const printGreeting = () => {
-    console.log('Hello there!'); 
-  }
-  printGreeting();
-
-const printMessage = org => {
-    console.log(`${org} is awesome!`);
-}
-printMessage("freeCodeCamp");
-
-const addTwoNumbers = (num1,num2) => (num1+num2);
-console.log(addTwoNumbers(3,4));
-
-const array = [1,2,3]
-
-console.log(array.every())
+console.log(Math.floor(Math.random()*6)+1)
