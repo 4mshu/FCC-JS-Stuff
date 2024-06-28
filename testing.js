064a3b0cb6616a1be8897a7dedc2d39d1c6ffd2d
4mshu <2hungrygators@gmail.com>
2024-06-28 13:59:34 +0545

    skeleton for cash register

--- testing.js
+++ testing.js
@@ -1,9 +1,14 @@
-let total =40
-const updateScore = (selectedValue,achieved) => {
-    total +=parseInt(selectedValue);
-    console.log(total);
-}
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
 
-updateScore("20",20)
-
-
+console.log(cid[0][1]/(0.01))
