1d189419b913e0591030ab88a0c077cd15248819
4mshu <2hungrygators@gmail.com>
2024-06-30 22:23:51 +0545

    code refactor: status message added, need to add denomination logic

--- Cash Register/script.js
+++ Cash Register/script.js
@@ -1,177 +1,47 @@
-let price = 19.5;
-let cid = [
-    ['PENNY', 1.01],
-    ['NICKEL', 2.05],
-    ['DIME', 3.1],
-    ['QUARTER', 4.25],
-    ['ONE', 90],
-    ['FIVE', 55],
-    ['TEN', 20],
-    ['TWENTY', 60],
-    ['ONE HUNDRED', 100]
-  ];
-const cash = document.getElementById("cash")
 const purchaseBtn = document.getElementById("purchase-btn")
-const changeDue = document.getElementById("change-due")
+const changeMsg = document.getElementById("change-due")
 
-const checkStatus = (price,cash) => {
+let price = 10;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+
+function cashInDrawer(arr){
     let sum = 0
-    cid.forEach((el)=> {
-        sum += el[1];
-    }) 
-    if(sum > (cash-price)){
-        changeDue.textContent = "Status: OPEN "
-        calculateChange(cash,price)
-    } else {
-        changeDue.textContent = "Status: INSUFFICIENT_FUNDS"
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i][1];
     }
+    return sum.toFixed(2);
 }
 
-const changeMsg = (arr,change) => {
-    let countOfChange = arr.reduce((acc, current) => {
-        if (acc[current]) {
-            acc[current]++;
-        } else {
-            acc[current] = 1;
-        }
-        return acc;
-    }, {});
-    for (let key in countOfChange){
-        if(key === "hundred"){
-            changeDue.textContent += `HUNDRED: $${countOfChange[key]*100} `
-        }
-        if(key === "twenty"){
-            changeDue.textContent += `TWENTY: $${countOfChange[key]*20} `
-        }
-        if(key === "ten"){
-            changeDue.textContent += `TEN: $${countOfChange[key]*10} `
-        }
-        if(key === "five"){
-            changeDue.textContent += `FIVE: $${countOfChange[key]*5} `
-        }
-        if(key === "one"){
-            changeDue.textContent += `ONE: $${countOfChange[key]*1} `
-        }
-        if(key === "quarter"){
-            changeDue.textContent += `QUARTER: $${countOfChange[key]*0.25} `
-        }
-        if(key === "dime"){
-            changeDue.textContent += `DIME: $${countOfChange[key]*0.1} `
-        }
-        if(key === "nickel"){
-            changeDue.textContent += `NICKEL: $${countOfChange[key]*0.05} `
-        }
-        if(key === "penny"){
-            changeDue.textContent += `PENNY: $${countOfChange[key]*0.01} `
-        }
-    }
+function calculateChange (changeDue){
+    
 
-}
-
-const calculateChange = (cash,price) => {
-    const change = (cash - price).toFixed(2);
-    let remainingChange = change
-    let changeArr = [];
-    while (cid[8][1] > 0 ){
-        if(remainingChange / 100 >= 1){
-            changeArr.push("hundred")
-            cid[8][1] -= (100).toFixed(2);
-            remainingChange -= (100).toFixed(2)
-        } else {
-            break;
-        }
-    } 
-
-    while (cid[7][1] > 0 ){
-        if(remainingChange / 20 >= 1){
-            changeArr.push("twenty")
-            cid[7][1] -= (20).toFixed(2);
-            remainingChange -= (20).toFixed(2)
-        }
-        else {
-            break;
-        }
-    }
-
-    while (cid[6][1] > 0 ){
-        if(remainingChange / 10 >= 1){
-            changeArr.push("ten")
-            cid[6][1] -= (10).toFixed(2);
-            remainingChange -= (10).toFixed(2)
-        }         else {
-            break;
-        }
-    }
-
-    while (cid[5][1] > 0 ){
-        if(remainingChange / 5 >= 1){
-            changeArr.push("five")
-            cid[5][1] -= (5).toFixed(2);
-            remainingChange -= (5).toFixed(2)
-        }         else {
-            break;
-        }
-    }
-
-    while (cid[4][1] > 0 ){
-        if(remainingChange / 1 >= 1){
-            changeArr.push("one")
-            cid[4][1] -= 1;
-            remainingChange -= (1).toFixed(2)
-        } else {
-            break;
-        }
-    }
-
-    while (cid[3][1] > 0 ){
-        if(remainingChange / 0.25 >= 1){
-            changeArr.push("quarter")
-            cid[3][1] -= 0.25;
-            remainingChange -= (0.25).toFixed(2)
-        } else {
-            break;
-        }
-    }
-
-    while (cid[2][1] > 0 ){
-        if(remainingChange / 0.1 >= 1){
-            changeArr.push("dime")
-            cid[2][1] -= (0.10).toFixed(2)
-            remainingChange -= (0.1).toFixed(2)
-        } else {
-            break;
-        }
-    }
-
-    while (cid[1][1] > 0 ){
-        if(remainingChange / 0.05 >= 1){
-            changeArr.push("nickel")
-            cid[1][1] -= (0.05).toFixed(2);
-            remainingChange -= (0.05).toFixed(2)
-        }        else {
-            break;
-        }
-    }
-
-    while (cid[0][1] > 0 ){
-        if(remainingChange.toFixed(2) / 0.01 >= 1){
-            changeArr.push("penny")
-            cid[0][1] -= (0.01).toFixed(2);
-            remainingChange -= (0.01).toFixed(2)
-        } else {
-            break;
-        }
-    }
-        changeMsg(changeArr,change)
 }
 
 purchaseBtn.addEventListener("click", ()=>{
-    if(cash.value<price){
-        alert("Customer does not have enough money to purchase the item");
-    } else if (cash.value == price){
-        changeDue.textContent = "No change due - customer paid with exact cash"
+    const cash = document.getElementById("cash")
+    const changeDue = cash.value - price;
+
+
+    if(changeDue < 0){
+        alert("Customer does not have enough money to purchase the item")
+    } else if (changeDue == 0){
+        changeMsg.textContent = "No change due - customer paid with exact cash";   
+    } else if (changeDue == cashInDrawer(cid)){
+        changeMsg.textContent = `Status: CLOSED ${calculateChange(changeDue)}`; 
     }
-    else {
-    checkStatus((price).toFixed(2),(cash.value));
+    else if (changeDue > cashInDrawer(cid)){
+        changeMsg.textContent = "Status: INSUFFICIENT_FUNDS";
+    } else {
+        changeMsg.textContent = `Status: OPEN ${calculateChange(changeDue)}`;
     }
 })
